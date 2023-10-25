2a7f8677dc911aa75d1674027411bedc3c912f60
Enea Jorgji <enea.jorgji20@gmail.com>
2023-10-25 18:23:53 -0400

    aws backend

--- app/api/watch_routes.py
+++ app/api/watch_routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from ..models import Review, db, Watch
 from datetime import datetime
 from flask_login import current_user, login_user, logout_user, login_required
 from ..forms.watch_form import WatchForm
 from .auth_routes import validation_errors_to_error_messages
 from ..forms.search_form import SearchForm
+from .aws import get_unique_filename, upload_file_to_s3
 
 watch_routes = Blueprint('watches', __name__)
 
@@ -48,14 +49,28 @@ def add_watch():
     form = WatchForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
-    if form.validate():
+
+
+    if form.validate_on_submit():
+        image = form.data["image_url"]
+        image.filename = get_unique_filename(image.filename)
+        upload = upload_file_to_s3(image)
+        print(upload)
+
+        # if "url" not in upload:
+        # # if the dictionary doesn't have a url key
+        # # it means that there was an error when you tried to upload
+        # # so you send back that error message (and you printed it above)
+        #     return render_template("post_form.html", form=form, errors=[upload])
+
+
         new_watch = Watch(
         brand = form.brand.data,
         model_name = form.model_name.data,
         price = form.price.data,
         about = form.about.data,
         description = form.description.data,
-        image_url = form.image_url.data,
+        image_url = upload['url'],
         owner_id = current_user.id,
         created_at=datetime.utcnow(),
         updated_at=datetime.utcnow()
@@ -77,10 +92,7 @@ def add_watch():
 @watch_routes.route('/<int:id>/edit', methods=['PUT'])
 @login_required
 def edit_watch(id):
-    form = WatchForm()
-
     watch_to_update = Watch.query.get(id)
-    form['csrf_token'].data = request.cookies['csrf_token']
 
     if not watch_to_update:
         return jsonify({"error": "Watch not found"}), 404
@@ -88,16 +100,71 @@ def edit_watch(id):
     if current_user.id != watch_to_update.owner_id:
         return jsonify({"error": "Unauthorized to edit this watch"}), 403
 
-    if form.validate_on_submit():
-        attributes_to_update = ['brand', 'model_name', 'price', 'about', 'description', 'image_url']
-        for attr in attributes_to_update:
-            if hasattr(form, attr):
-                setattr(watch_to_update, attr, getattr(form, attr).data)
-        db.session.commit()
+    # Check if an image is included in the request
+    if 'image_url' in request.files and request.files['image_url']:
+        new_image = request.files['image_url']
+        new_image.filename = get_unique_filename(new_image.filename)
+        upload = upload_file_to_s3(new_image)
 
-        return {"updated_watch": watch_to_update.to_dict()}, 200
+        if 'url' in upload:
+            # Update the image URL if the upload was successful
+            watch_to_update.image_url = upload['url']
 
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+    # Update other watch attributes
+    watch_to_update.brand = request.form['brand']
+    watch_to_update.model_name = request.form['model_name']
+    watch_to_update.price = request.form['price']
+    watch_to_update.about = request.form['about']
+    watch_to_update.description = request.form['description']
+
+    db.session.commit()
+
+    return {"Updated_watch": watch_to_update.to_dict()}
+# @watch_routes.route('/<int:id>/edit', methods=['PUT'])
+# @login_required
+# def edit_watch(id):
+#     form = WatchForm()
+
+#     watch_to_update = Watch.query.get(id)
+#     form['csrf_token'].data = request.cookies['csrf_token']
+
+#     if not watch_to_update:
+#         return jsonify({"error": "Watch not found"}), 404
+
+#     if current_user.id != watch_to_update.owner_id:
+#         return jsonify({"error": "Unauthorized to edit this watch"}), 403
+
+#     if form.validate_on_submit():
+#         if 'image_url' in form.data and form.data['image_url'] is not None:
+#             new_image = form.data['image_url']
+#             new_image.filename = get_unique_filename(new_image.filename)
+#             upload = upload_file_to_s3(new_image)
+
+#         if 'url' in upload:
+#             # Update the image URL if the upload was successful
+#             watch_to_update.image_url = upload['url']
+            
+#         # image = form.data["image_url"]
+#         # image.filename = get_unique_filename(image.filename)
+#         # upload = upload_file_to_s3(image)
+#         # print(upload)
+#         # attributes_to_update = ['brand', 'model_name', 'price', 'about', 'description', 'image_url']
+#         # for attr in attributes_to_update:
+#         #     if hasattr(form, attr):
+#         #         setattr(watch_to_update, attr, getattr(form, attr).data)
+#         # db.session.commit()
+#         watch_to_update.brand = form.brand.data
+#         watch_to_update.model_name = form.model_name.data
+#         watch_to_update.price = form.price.data,
+#         watch_to_update.about = form.about.data,
+#         watch_to_update.description = form.description.data
+#         # watch_to_update.image_url = upload['url']
+
+#         db.session.commit()
+
+#         return {"Updated_watch": watch_to_update.to_dict()}
+#     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
     #     watch_to_update.brand = form.data.brand
     #     watch_to_update.model_name = form.data.model_name
@@ -106,8 +173,6 @@ def edit_watch(id):
     #     watch_to_update.description = form.data.description
     #     watch_to_update.image_url = form.data.image_url
 
-    #     return {"Updated_watch": watch_to_update.to_dict()}
-    # return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 #Delete a watch
--- app/forms/watch_form.py
+++ app/forms/watch_form.py
@@ -3,14 +3,13 @@ from wtforms import StringField, IntegerField, DecimalField
 from wtforms.validators import DataRequired, Length, ValidationError, NumberRange
 from app.models import Watch
 from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.aws import ALLOWED_EXTENSIONS
 
 
-ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
-
-def valid_image_url(form, field):
-    file = field.data
-    if ('.' in file and file.rsplit('.', 1)[1].lower() not in ALLOWED_EXTENSIONS) or ('.' not in file):
-        raise ValidationError("Invalid Image File Extension")
+# def valid_image_url(form, field):
+#     file = field.data
+#     if ('.' in file and file.rsplit('.', 1)[1].lower() not in ALLOWED_EXTENSIONS) or ('.' not in file):
+#         raise ValidationError("Invalid Image File Extension")
 
 class WatchForm(FlaskForm):
     brand = StringField('Brand', validators=[DataRequired(),Length(max=500)])
@@ -18,5 +17,5 @@ class WatchForm(FlaskForm):
     price = DecimalField('Price', places=2, rounding=None, number_format='%.7f', validators=[DataRequired(), NumberRange(min=1)])
     about = StringField('About', validators=[DataRequired(),Length(max=500)])
     description = StringField('Description', validators=[DataRequired(),Length(max=500)])
-    image_url = StringField("Image_url", validators=[DataRequired(), valid_image_url])
+    image_url = FileField("Item Image", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     # cart_id = IntegerField('Cart ID', validators=[NumberRange(min=1, message='Cart ID must be a positive integer')])
