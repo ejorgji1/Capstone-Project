1985e5805040f1f27e38a0f6483fb2db91ad53de
Enea Jorgji <enea.jorgji20@gmail.com>
2024-01-02 22:02:45 -0500

    added seeders

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -10,10 +10,34 @@ def seed_users():
         username='marniemorine', email='marnie@aa.io', first_name='Marnie', last_name='Mornie', password='password1')
     bobbie = User(
         username='bobbiebuild', email='bobbie@aa.io',first_name='Bobbie', last_name='Builder', password='password2')
+    sal_mon = User(
+        username='river_swimmer', email='fish@ocean.io', first_name='Sal', last_name='Mon', password='password56')
+    rob_in = User(
+        username='bird_lover', email='nature@fly.io', first_name='Rob', last_name='In', password='password57')
+    grace_full = User(
+        username='elegant_dancer', email='ballet@move.io', first_name='Grace', last_name='Full', password='password58')
+    brooke_lyn = User(
+        username='ny_resident', email='newyork@bridge.io', first_name='Brooke', last_name='Lyn', password='password59')
+    amber_alert = User(
+        username='safety_first', email='emergency@warning.io', first_name='Amber', last_name='Alert', password='password60')
+    earl_y = User(
+        username='morning_person', email='dawn@rise.io', first_name='Earl', last_name='Y', password='password61')
+    mae_flower = User(
+        username='pilgrim_ship', email='voyages@sea.io', first_name='Mae', last_name='Flower', password='password62')
+    will_power = User(
+        username='inner_strength', email='determination@resolve.io', first_name='Will', last_name='Power', password='password63')
 
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(sal_mon)
+    db.session.add(rob_in)
+    db.session.add(grace_full)
+    db.session.add(brooke_lyn)
+    db.session.add(amber_alert)
+    db.session.add(earl_y)
+    db.session.add(mae_flower)
+    db.session.add(will_power)
     db.session.commit()
 
 
--- app/seeds/watches.py
+++ app/seeds/watches.py
@@ -63,9 +63,119 @@ def seed_watches():
         created_at=datetime.datetime.now(),
         updated_at=datetime.datetime.now()
     )
+    watch6 = Watch(
+        brand="Audemars Piguet",
+        model_name="Royal Oak",
+        price=25000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/audemars-piguet/images/Used-Audemars-Piguet-Royal-Oak-15202ST.OO.1240ST.01.A-SKU157239.jpg",
+        owner_id=6,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch7 = Watch(
+        brand="Rolex",
+        model_name="Explorer",
+        price=10000,
+        about="This watch is in overall excellent condition.",
+        description="White w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Explorer-II-226570-SKU161679.jpg",
+        owner_id=7,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch8 = Watch(
+        brand="Rolex",
+        model_name="Submariner",
+        price=12000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Submariner-126613-SKU161871.jpg",
+        owner_id=8,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch9 = Watch(
+        brand="Omega",
+        model_name="Speedmaster",
+        price=5250,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/omega/images/Used-Omega-Speedmaster-3577.50.00-SKU160655.jpg",
+        owner_id=9,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch10 = Watch(
+        brand="Rolex",
+        model_name="Yacht-Master",
+        price=10200,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Yacht-Master-116655-SKU-161025.jpg",
+        owner_id=10,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch11 = Watch(
+        brand="Rolex",
+        model_name="Sky-Dweller",
+        price=12500,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Sky-Dweller-326933-SKU161641.jpg",
+        owner_id=11,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch12 = Watch(
+        brand="Audemars Piguet",
+        model_name="Royal Oak",
+        price=25000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/audemars-piguet/images/Used-Audemars-Piguet-Royal-Oak-Offshore-26170ST.OO.D101CR.02-SKU161253.jpg",
+        owner_id=7,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch13 = Watch(
+        brand="Rolex",
+        model_name="GMT-MASTER 2",
+        price=13000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-GMT-Master-II-126711-SKU161711.jpg",
+        owner_id=8,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch14 = Watch(
+        brand="Rolex",
+        model_name="Oyster Perpetual",
+        price=4000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Oyster-Perpetual-177200-SKU161543.jpg",
+        owner_id=9,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
+    watch15 = Watch(
+        brand="Rolex",
+        model_name="Daytona",
+        price=11000,
+        about="This watch is in overall excellent condition.",
+        description="Blue w/ luminous hour markers and snowflake hands",
+        image_url="https://www.bobswatches.com/images/Used-Rolex-Daytona-116523-SKU159784.jpg",
+        owner_id=2,
+        created_at=datetime.datetime.now(),
+        updated_at=datetime.datetime.now()
+    )
 
     db.session.add_all([
-    watch1, watch2, watch3, watch4, watch5
+    watch1, watch2, watch3, watch4, watch5,watch6,watch7,watch8,watch9,watch10,watch11,watch12,watch13,watch14,watch15,
     ])
 
     db.session.commit()
