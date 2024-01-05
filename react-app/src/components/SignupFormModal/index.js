2d29bee0c66a75c1032d8400fd145bed8187acdf
Enea Jorgji <enea.jorgji20@gmail.com>
2024-01-04 22:22:54 -0500

    update singup modal

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
@@ -16,30 +16,70 @@ function SignupFormModal() {
 	const [errors, setErrors] = useState([]);
 	const { closeModal } = useModal();
 
+	useEffect(() => {
+		const errors = {};
+		if (username.length === 0) errors.username = "";
+		if (email.length === 0) errors.email = "";
+		if (first_name.length === 0) errors.firstName = "";
+		if (last_name.length === 0) errors.lastName = "";
+		if (password.length === 0 || password.length < 6) errors.password = "";
+		if (confirmPassword.length === 0 || confirmPassword.length < 6)
+		  errors.confirmPassword = "";
+		if (username.length < 4 && username.length > 0)
+		  errors.username = "Username needs to be at least 4 characters.";
+		if (password.length < 6 && password.length > 0)
+		  errors.password = "Password needs to be at least 6 characters.";
+	
+		setErrors(Object.values(errors));
+	  }, [email, first_name, last_name, username, password, confirmPassword]);
+
+	
+
 	const isValidEmail = (email) => {
 		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 		return emailRegex.test(email);
 	  };
 
+	// const handleSubmit = async (e) => {
+	// 	e.preventDefault();
+	// 	if (password === confirmPassword) {
+	// 		const data = await dispatch(signUp(username, email, password,first_name, last_name));
+	// 		if (data) {
+	// 			setErrors(data);
+	// 		} else {
+	// 			closeModal();
+	// 		}
+	// 	} else {
+	// 		setErrors([
+	// 			"Passwords must match",
+	// 		]);
+	// 	}
+
+	// 	if (!isValidEmail(email)) {
+	// 		setErrors(['Invalid email format']);
+	// 		return;
+	// 	  }
+	// };
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password,first_name, last_name));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-			}
-		} else {
-			setErrors([
-				"Passwords must match",
-			]);
+	
+		if (password !== confirmPassword) {
+			setErrors(["Passwords must match"]);
+			return; 
 		}
-
+	
 		if (!isValidEmail(email)) {
-			setErrors(['Invalid email format']);
-			return;
-		  }
+			setErrors(["Invalid email format"]);
+			return; 
+		}
+	
+		const data = await dispatch(signUp(username, email, password, first_name, last_name));
+		if (data) {
+			setErrors(data);
+		} else {
+			closeModal();
+		}
 	};
 
 	return (
